b4105704dbcf138d924ca6ead3fff0329bfd45a3
BimalkaMuhandiram <164906917+BimalkaMuhandiram@users.noreply.github.com>
2024-11-01 15:41:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
 import joblib
 import warnings
@@ -10,30 +9,6 @@ warnings.filterwarnings("ignore")
 
 # Load the trained model
 model = joblib.load('random_forest_regressor_model.pkl')
-
-# Preprocessing function
-def preprocess_data(input_data):
-    # Normalize column names to lowercase
-    input_data.columns = input_data.columns.str.lower()
-
-    # Convert dates to datetime objects
-    input_data['order_date'] = pd.to_datetime(input_data['order_date'])
-    input_data['ship_date'] = pd.to_datetime(input_data['ship_date'])
-
-    # Encode categorical columns
-    categorical_columns = ['ship_mode', 'segment', 'region', 'category', 'sub-category', 'customer_id', 'customer_name']
-    le = LabelEncoder()
-    
-    for col in categorical_columns:
-        if col in input_data.columns:
-            input_data[col] = input_data[col].fillna("Unknown")  # Fill missing values
-            input_data[col] = le.fit_transform(input_data[col])  # Label encode categorical variables
-
-    # Handle missing numeric values
-    numeric_cols = input_data.select_dtypes(include=['float64', 'int64']).columns
-    input_data[numeric_cols] = input_data[numeric_cols].fillna(0)  # Fill numeric missing values with 0
-
-    return input_data
 
 # Streamlit app
 st.title("Sales Prediction App")
@@ -81,8 +56,7 @@ if st.button("Predict"):
     # Preprocess input data
     preprocessed_data = preprocess_data(input_data)
 
-    # Make sure to only use the features that the model was trained on
-    # Define the expected columns for the model (without 'sales')
+    # Ensure to use only features that the model was trained on
     expected_columns = ['row_id', 'order_id', 'order_date', 'ship_date', 
                         'ship_mode', 'customer_id', 'customer_name', 
                         'segment', 'city', 'state', 'postal_code', 
